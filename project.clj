85fca29a7428172a9d2e1f607a18fb2696ca7a2b
Joe Gallo <joegallo@gmail.com>
2010-09-25 17:35:58 -0400

    Updated to reflect the current state of the art.

--- project.clj
+++ project.clj
@@ -2,9 +2,5 @@
   :description "Clojure Robots"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 ;;  [robocode "1.7.0.2"]
-		 [org.nakkaya.robocode/robocode "1.7.2.0"]
-		 ]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :manifest {"robots" "robots.fatrobot"}
-  :aot [robots.fatrobot])
+		 [org.joegallo.robocode/robocode "1.7.0.2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/robots/core.clj
+++ src/robots/core.clj
@@ -1,14 +1,54 @@
 (ns robots.core
-  (:use [robots.engine :only [engine run-battle]]))
+  (:import [java.io File]
+           [robocode.control RobocodeEngine BattlefieldSpecification BattleSpecification]
+           [robocode.control.events BattleAdaptor]))
 
-(def eng (engine "/home/joe/robocode/" true))
+;; nosecurity allows clojure robots to work, robotpath tells robocode
+;; where our clojure robots are located
+(System/setProperty "NOSECURITY" "true")
+(System/setProperty "ROBOTPATH" "classes")
 
-(do 
-  ;; (compile 'robots.fatrobot)
-  (run-battle eng 3 [800 600] 
-	      ["sample.SittingDuck" 
-	       "sample.Fire" 
-	       "sample.Crazy"
-	       "sample.RamFire" 
-	       ;;"robots.fatrobot*"
-	       ]))
+;; TODO add your robot here
+(compile 'robots.fatrobot)
+;; (compile 'your.robot.here)
+
+(defn battle-console []
+  (proxy [BattleAdaptor] []
+    (onBattleMessage [e] (println "Msg> " (.getMessage e)))
+    (onBattleError [e] (println "Err> " (.getError e)))
+    (onBattleCompleted
+     [e]
+     (println "\n\n-- Battle Complete --\n")
+     (doseq [result (.getSortedResults e)]
+       (println (.getTeamLeaderName result) (.getScore result))))))
+
+(defn engine []
+  (doto (RobocodeEngine.)
+    (.setVisible true)
+    (.addBattleListener (battle-console))))
+
+(defn battle [engine rounds size robots]
+  (let [[x y] size
+        field (BattlefieldSpecification. x y)]
+    (BattleSpecification.
+     rounds field
+     (into-array
+      (reduce (fn [h v]
+                (conj h (first (.getLocalRepository engine v))))
+              [] robots)))))
+
+(defn run-battle
+  ([engine rounds size robots & wait]
+     (run-battle engine (battle engine rounds size robots))
+     (if (not (nil? wait))
+       (.waitTillBattleOver engine)))
+  ([engine battle]
+     (.runBattle engine battle false)))
+
+;; this is the main entry point
+;; call this with robots as string names
+(defn fight [& robots]
+  (run-battle (engine)
+              3 ;; # rounds
+              [800 600] ;; battle size
+              robots))
