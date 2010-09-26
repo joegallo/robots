e832c3ebbb46b73918892af74730f2023bcde84f
Joe Gallo <joegallo@gmail.com>
2010-09-25 20:25:46 -0400

    Add lein run as a depedency.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.joegallo.robocode/robocode "1.7.1.6"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-run "1.0.0"]])
